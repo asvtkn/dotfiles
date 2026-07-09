ceb38e327ccfa14a75c43866d91bbf847a154146
alex <alexsvat@pm.me>
2026-07-09 08:40:56 -0700

    changes

--- config/nvim/lua/plugins/fzf-lua.lua
+++ config/nvim/lua/plugins/fzf-lua.lua
@@ -40,22 +40,19 @@ return {
         }
     end,
     keys = {
-        { "<leader>f:", "<cmd>FzfLua command_history<cr>",                          desc = "Command History" },
         -- find
-        { "<leader>ff", "<cmd>FzfLua live_grep<cr>",                                desc = "Live Grep" },
-        { "<leader>fo", "<cmd>FzfLua files<cr>",                                    desc = "Files" },
-        { "<leader>fb", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
-        { "<leader>fg", "<cmd>FzfLua git_files<cr>",                                desc = "Git Files" },
-        { "<leader>fr", "<cmd>FzfLua oldfiles<cr>",                                 desc = "Recent(Old) files" },
-        { "<leader>fc", "<cmd>FzfLua colorschemes<cr>",                             desc = "Colorschemes" },
+        { "<leader>f", "<cmd>FzfLua live_grep<cr>",                                 desc = "Live Grep" },
+        { "<leader>Fb", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
+        { "<leader>Fg", "<cmd>FzfLua git_files<cr>",                                desc = "Git Files" },
+        { "<leader>Fr", "<cmd>FzfLua oldfiles<cr>",                                 desc = "Recent(Old) files" },
+        { "<leader>kt", "<cmd>FzfLua colorschemes<cr>",                             desc = "Colorschemes" },
         -- git
-        { "<leader>fgc", "<cmd>FzfLua git_commits<CR>",                              desc = "Commits" },
-        { "<leader>fgs", "<cmd>FzfLua git_status<CR>",                               desc = "Status" },
+        { "<leader>gc", "<cmd>FzfLua git_commits<CR>",                               desc = "Commits" },
+        { "<leader>gs", "<cmd>FzfLua git_status<CR>",                                desc = "Status" },
         -- search
         { '<leader>s"', "<cmd>FzfLua registers<cr>",                                desc = "Registers" },
         { "<leader>sa", "<cmd>FzfLua autocmds<cr>",                                 desc = "Auto Commands" },
-        { "<leader>sb", "<cmd>FzfLua grep_curbuf<cr>",                              desc = "Buffer" },
-        { "<leader>sc", "<cmd>FzfLua command_history<cr>",                          desc = "Command History" },
+        { "<leader>Fc", "<cmd>FzfLua command_history<cr>",                          desc = "Command History" },
         { "<leader>sC", "<cmd>FzfLua commands<cr>",                                 desc = "Commands" },
         { "<leader>sd", "<cmd>FzfLua diagnostics_document<cr>",                     desc = "Document Diagnostics" },
         { "<leader>sD", "<cmd>FzfLua diagnostics_workspace<cr>",                    desc = "Workspace Diagnostics" },
@@ -66,7 +63,7 @@ return {
         { "<leader>sl", "<cmd>FzfLua loclist<cr>",                                  desc = "Location List" },
         { "<leader>sM", "<cmd>FzfLua man_pages<cr>",                                desc = "Man Pages" },
         { "<leader>sm", "<cmd>FzfLua marks<cr>",                                    desc = "Jump to Mark" },
-        { "<leader>sR", "<cmd>FzfLua resume<cr>",                                   desc = "Resume" },
+        { "<leader>FF", "<cmd>FzfLua resume<cr>",                                   desc = "Resume" },
         { "<leader>sq", "<cmd>FzfLua quickfix<cr>",                                 desc = "Quickfix List" },
     },
 }
